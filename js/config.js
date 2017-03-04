072fe376d5366cb255791560e01110228526ebde
henchan <henchan@Henchans-iMac.local>
2017-03-04 19:26:34 +0900

    .

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ configReactDriveCms = {
     /**
      * Add the Id for the Google Sheets Dashboard file
      */
-    dashboardId: '11I0ytcJ2w3hFczzOsp03BZYC0ZN5iRzKgwc0tFDMOUw',
+    dashboardId: '11I0ytcJ2w3hFczzOsp03BZYC0ZN5iRzKgwc0tFDMOUwXXX',
 
     /**
      * Add the Url Id for the Google Web App Script to forward email from the Contact Form
