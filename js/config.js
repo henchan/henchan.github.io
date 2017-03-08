7d2e04089e30225ae39a0bfb4402101a896e12f8
henchan <henchan@Henchans-iMac.local>
2017-03-08 10:20:21 +0900

    .

--- js/config.js
+++ js/config.js
@@ -3,8 +3,11 @@ configReactDriveCms = {
     /**
      * Add the Id for the Google Sheets Dashboard file
      */
-    dashboardId: '1L8-9OQHn0h_3AZsVsp_-563MMZhOl6aqcl6zGkIsXNE',
 
+    dashboardId: '1hBDOdFlc6OmQI5gKIkpGsNtbRTgpubM5EBH2qUK9EOU', // henchan
+/*
+    dashboardId: '1L8-9OQHn0h_3AZsVsp_-563MMZhOl6aqcl6zGkIsXNE', // communacado
+*/
     /**
      * Add the Url Id for the Google Web App Script to forward email from the Contact Form
      */
