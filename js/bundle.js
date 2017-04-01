727a188c74ef9360b7395e257ee0e50ad4c3483a
henchan <henchan@Henchans-iMac.local>
2017-04-01 17:40:45 +0900

    .

--- js/bundle.js
+++ js/bundle.js
@@ -46178,12 +46178,6 @@ var App = function (drive, views, route) {
                     break;
             }
         },
-
-
-    listenPressed: function (event) {
-        alert ("listen. we got a guid: ");
-    },
-       
         handleRouting: function (element, e) {
             e.preventDefault();
             e.stopPropagation();
@@ -47634,8 +47628,7 @@ var Drive = function (config, $) {
                                 guid = htmlStr.slice( nextHref + guidPos ,  nextHref + guidPos + guidLength);
                             }
                             newHtmlStr = htmlStr.slice(0, nextHref + hrefOpenStr.length) +
-'"#" onclick= alert(99);' +
-// "javascript:alert(0); " +
+ "javascript:alert(0); " +
 //"javascript:{this." + button.onClick + "} " +
                             // guid ?
                             //    "javascript:" + button.onClick + "('"+guid+"'); " :
